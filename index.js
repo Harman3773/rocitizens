ceee91e29f395a607f654202b5876fb2797a8046
discordmania <41619492+discordmania@users.noreply.github.com>
2018-09-18 22:13:03 +0100

    Update index.js

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ bot.on("message", async message => {
        let warnreason = args.join(" ").slice(22);
         
         let warnembed = new Discord.RichEmbed()
-        .setThumbnail("https://cdn.discordapp.com/attachments/491640462365556747/491643357341417482/3602266913a7a394fb5f4d1e3061bd03.png")
+        .setThumbnail("https://cdn.discordapp.com/attachments/426090985475538954/491717416355889172/Possible_AFPU.png")
         .setColor("#2A363B")
         .addField("Warn Reason:", warnreason)
         .setDescription("You have been warned in ***AFPU: Communications.***")
@@ -66,7 +66,7 @@ if(cmd === `${prefix}shout`){
      let embed = new Discord.RichEmbed()
      .setDescription(announcement)
      .setTitle("AFPU: Announcement.")
-     .setThumbnail("https://cdn.discordapp.com/attachments/491640462365556747/491643357341417482/3602266913a7a394fb5f4d1e3061bd03.png")
+     .setThumbnail("https://cdn.discordapp.com/attachments/426090985475538954/491717416355889172/Possible_AFPU.png")
      .setColor("#2A363B")
       message.delete();
      message.channel.send(embed)
@@ -81,6 +81,7 @@ if(cmd === `${prefix}shout`){
       .setDescription("These gate keys are not to be leaked ANYWHERE, if we find out you have leaked them there will be serious consequences.")
       .addField("Earth", "1 20 2 21 3 22")
       .addField("Gamma", "1 2 3 4 5 6")
+      .setThumbnail("https://cdn.discordapp.com/attachments/426090985475538954/491717416355889172/Possible_AFPU.png")
       .addField("CLASSIFIED", "##############")
       .addField("CLASSIFIED", "##############")
       .addField("CLASSIFIED", "##############")
@@ -149,17 +150,7 @@ if(cmd === `${prefix}shout`){
  }    
       
  //---------------------------------------------------------------\\//---------------------------------------------------------------\\ 
-     
- if(cmd === `${prefix}matcominfo`){
-   let embed = new Discord.RichEmbed()
-   .setTitle("MATCOM rank job information")
-   .setDescription("To join MATCOM, apply when the apps are open.")
-   .addField("SGSOC job:", "The MATCOM (Material Command) The MATCOM are incharge of development.")
-   .setFooter("MATCOM  - Material command.")
-   .setThumbnail("https://cdn.discordapp.com/attachments/491640462365556747/491658524405792770/a21c8dc10b0cbf544f3f05df42d8c8a6.png")
-   .setColor("#2A363B")
-   message.channel.send(embed)
- }    
+   
       
  //---------------------------------------------------------------\\//---------------------------------------------------------------\\ 
       
@@ -177,7 +168,7 @@ if(cmd === `${prefix}shout`){
  //---------------------------------------------------------------\\//---------------------------------------------------------------\\
       
       if(cmd === `${prefix}lander`){
-          message.reply("LANDER IS THE GOD DAMN BEST PERSON.")
+          message.reply("Lander is terrible.")
       }            
       
  //---------------------------------------------------------------\\//---------------------------------------------------------------\\
@@ -232,11 +223,11 @@ if (cmd === `${prefix}report`){
    .setDescription("You have been reported, please bear in mind the administrators now have this report file Thanks.")
    .addField("Report reason:", reason)
    .setColor("#2A363B")
-   .setThumbnail("https://cdn.discordapp.com/attachments/491640462365556747/491643357341417482/3602266913a7a394fb5f4d1e3061bd03.png")
+   .setThumbnail("https://cdn.discordapp.com/attachments/426090985475538954/491717416355889172/Possible_AFPU.png")
   
       
    let reportembed = new Discord.RichEmbed()
-   .setThumbnail("https://cdn.discordapp.com/attachments/491640462365556747/491643357341417482/3602266913a7a394fb5f4d1e3061bd03.png")   
+   .setThumbnail("https://cdn.discordapp.com/attachments/426090985475538954/491717416355889172/Possible_AFPU.png")   
    .setTitle("Moderation Report")
    .setDescription("Moderators need to act on the reported user.")
    .setColor("#2A363B")
@@ -273,11 +264,11 @@ if (cmd === `${prefix}report`){
    .setDescription("You have been kicked, please bear in mind the administrators now have this kick file Thanks.")
    .addField("Kick reason:", kReason)
    .setColor("#2A363B")
-   .setThumbnail("https://cdn.discordapp.com/attachments/491640462365556747/491643357341417482/3602266913a7a394fb5f4d1e3061bd03.png")      
+   .setThumbnail("https://cdn.discordapp.com/attachments/426090985475538954/491717416355889172/Possible_AFPU.png")      
         
         
   let kEmbed = new Discord.RichEmbed()
-   .setThumbnail("https://cdn.discordapp.com/attachments/491640462365556747/491643357341417482/3602266913a7a394fb5f4d1e3061bd03.png") 
+   .setThumbnail("https://cdn.discordapp.com/attachments/426090985475538954/491717416355889172/Possible_AFPU.png") 
    .setTitle("Moderation Kick")
   .setColor("#2A363B")
   .addField("Kicked User.", `${kUser} with the ID: ${kUser.id}`)
@@ -314,12 +305,12 @@ if (cmd === `${prefix}report`){
    .setDescription("You have been Banned, please bear in mind the administrators now have this kick file Thanks.")
    .addField("Ban reason:", bReason)
    .setColor("#2A363B")
-   .setThumbnail("https://cdn.discordapp.com/attachments/490597093971591172/490601114031357952/thDR3EMMI7.jpg")
+   .setThumbnail("https://cdn.discordapp.com/attachments/426090985475538954/491717416355889172/Possible_AFPU.png")
    
          
   
     let bEmbed = new Discord.RichEmbed()
-    .setThumbnail("https://cdn.discordapp.com/attachments/490597093971591172/490601114031357952/thDR3EMMI7.jpg")    
+    .setThumbnail("https://cdn.discordapp.com/attachments/426090985475538954/491717416355889172/Possible_AFPU.png")    
     .setTitle("Moderation Ban")
     .setColor("#2A363B")
     .addField("Banned User.", `${bUser} with the ID: ${bUser.id}`)
