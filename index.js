c906da3397aa727ecd5d08aef3d32e36f2130765
discordmania <41619492+discordmania@users.noreply.github.com>
2018-09-20 16:19:26 +0100

    Update index.js

--- index.js
+++ index.js
@@ -31,25 +31,7 @@ bot.on("message", async message => {
 }  
  
  //---------------------------------------------------------------\\//---------------------------------------------------------------\\
-  
- if(cmd === `${prefix}purge`){
-    // This command removes all messages from all users in the channel, up to 100.
     
-    // get the delete count, as an actual number.
-    const deleteCount = parseInt(args[0], 10);
-    
-    // Ooooh nice, combined conditions. <3
-    if(!deleteCount || deleteCount < 2 || deleteCount > 100)
-      return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
-    
-    // So we get our messages, and delete them. Simple enough, right?
-    const fetched = await message.channel.fetchMessages({limit: deleteCount});
-    message.channel.bulkDelete(fetched)
-      .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
-
- }
-      
- //---------------------------------------------------------------\\//---------------------------------------------------------------\\  
       if(cmd === `${prefix}warn`){
       
    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("***Sorry you cant warn users.***");    
