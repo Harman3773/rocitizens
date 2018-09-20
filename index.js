0f70a4efa37b0954dc855f9e73bc66ec876cf7f4
discordmania <41619492+discordmania@users.noreply.github.com>
2018-09-20 16:28:18 +0100

    Update index.js

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ bot.on("message", async message => {
  //---------------------------------------------------------------\\//---------------------------------------------------------------\\
   if(cmd === `${prefix}botstats`){
     let embed = new Discord.RichEmbed()
-    .setDescription("***AFPU Official Bot®***)
+    .setDescription("***AFPU Official Bot®***")
     .addField("Bot ID:", "491638483862159390")
     .addField("Bot Maker:", "<@481171799204429834>")
     
